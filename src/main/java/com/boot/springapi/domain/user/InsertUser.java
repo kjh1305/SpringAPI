49cecf9921c8163f8a288a29bbd0f8edc9870ac2
kjh1305 <cosyq1305@naver.com>
2021-07-27 18:08:39 +0900

    validation from add

--- src/main/java/com/boot/springapi/domain/user/InsertUser.java
+++ src/main/java/com/boot/springapi/domain/user/InsertUser.java
@@ -1,4 +1,24 @@
 package com.boot.springapi.domain.user;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class InsertUser {
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String name;
 }
