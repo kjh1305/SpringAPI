5b8477fd790ae649f9af983c8d223b51910aa00a
kjh1305 <cosyq1305@naver.com>
2021-08-01 02:22:53 +0900

    pagination block cnt update

--- src/main/java/com/boot/springapi/pagination/Pagination.java
+++ src/main/java/com/boot/springapi/pagination/Pagination.java
@@ -10,7 +10,7 @@ public class Pagination {
     private int pageSize = 5;
 
     /** 2. 페이징된 버튼의 블럭당 최대 개수 **/
-    private int blockSize = 10;
+    private int blockSize = 5;
 
     /** 3. 현재 페이지 **/
     private int page = 1;
