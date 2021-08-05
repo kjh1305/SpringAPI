297ac089582bf7995b6aec8a3444a76b93e92635
kjh1305 <cosyq1305@naver.com>
2021-08-06 00:29:39 +0900

    test delete

--- src/test/java/com/boot/springapi/SpringApiApplicationTests.java
+++ src/test/java/com/boot/springapi/SpringApiApplicationTests.java
@@ -1,17 +1,8 @@
 package com.boot.springapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootTest
 class SpringApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
 
 }
